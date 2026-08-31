e6acd0f15494c5e410c70ff40b4507abb5b16d10
Ziad-Yaseen <ziadyaseen.dev@gmail.com>
2026-08-31 10:58:53 +0300

    app localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/app.dart';
 
-void main() {
-  runApp(const News());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: const News(),
+    )
+  );
 }
